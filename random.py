d40309fd2fef5dae64673963a7b285b2f7eea2f3
Noku91 <elielchenje@riccarton.school.nz>
2025-03-18 10:14:47 +1300

    Initial commit

---
+++ random.py
@@ -0,0 +1,8 @@
+'''This program asks how many of each pizza should be orderd 
+and then prints out the orderd, skipping any pizza taht weren't ordered'''
+
+# Create list of pizza types 
+pizzas = ['cheese', 'pepperoni', 'veggie', 'chicken']
+# Go through list of pizzas in order 
+for pizza in pizzas:
+    print('pizza')
---
+++ wassup.py
@@ -0,0 +1,2 @@
+answer = int(input('Enter your input: '))
+
