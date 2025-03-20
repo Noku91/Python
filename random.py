52b23dc651ac7e91d21d532fb1455785cab02097
Noku91 <elielchenje@riccarton.school.nz>
2025-03-21 09:54:06 +1300

    NCEA practice

--- random.py
+++ random.py
@@ -4,5 +4,8 @@ and then prints out the orderd, skipping any pizza taht weren't ordered'''
 # Create list of pizza types 
 pizzas = ['cheese', 'pepperoni', 'veggie', 'chicken']
 # Go through list of pizzas in order 
+quantities = []
+
+
 for pizza in pizzas:
     print('pizza')
--- wassup.py
+++ wassup.py
@@ -1,2 +1,8 @@
-answer = int(input('Enter your input: '))
+'''This program asks the user to enter their input for each of a series of voltage values'''
+num = int(input('Enter your input: '))
+
+# print out the input
+if num = '-1':
+    print('Boop')
+else:
 
