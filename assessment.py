da3d7f5c37b6cbae6d0f186bf9db070e3cfd858e
Noku91 <elielchenje@riccarton.school.nz>
2025-03-25 09:43:28 +1300

    notsure.py

--- assessment.py
+++ assessment.py
@@ -3,5 +3,13 @@ angles = []
 current_angle = int(input('Enter your current angle: '))
 while current_angle <= 359:
     angles.append(current_angle)
-    
+
+    current_angle = int(input('Enter your current angle: '))
+
+for angle in current_angles:
+    if angle <= 359:
+        print('final')
+    else:
+        print('ERROR: INVALID ANGLE')   
+
 
