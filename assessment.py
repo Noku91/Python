9665c3b959b34a8214ef1adc9b73cc3140bb8810
Noku91 <elielchenje@riccarton.school.nz>
2025-03-26 14:29:05 +1300

    Bit of change-ish.py

--- assessment.py
+++ assessment.py
@@ -2,9 +2,16 @@ runway_number = []
 
 while True:
     try:
-        angle = input('Enter your current angle: ')
+        angle_current = input('Enter your current angle: ')
 
-if angle.lower()
+if angle_current.lower() == 'final':
+    break
+runway_number.append(angle)
+
+expect ValueError:
+print('')
+
+
 
 
 
