e4fbb7ecada708f938cf64d7a8af86747df63296
Noku91 <elielchenje@riccarton.school.nz>
2025-03-28 10:09:27 +1300

    nvmifoundwhatswrong.py

--- assessment.py
+++ assessment.py
@@ -1,11 +1,12 @@
 def get_runway(angle):
     runway= (angle + 4) //10
     if runway == 36:
-    return 0
+        
+        return 0
     return runway
 
 runway_numbers = []
-while true:
+while True:
     angle_input = input("Enter your current angle: ")
     if angle_input.lower() == "final":
         break
@@ -18,7 +19,3 @@ while true:
             print("ERROR: INVALID ERROR")
     except ValueError:
         print("ERROR: INVALID ERROR")
-
-        
-
-
